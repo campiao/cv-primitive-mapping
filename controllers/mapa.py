83b997b70001ecf1f2cca99e2ef08e6921e8a752
Guilherme Vaz <guivaz8@gmail.com>
2024-06-11 22:45:59 +0100

    mapa final quase quasitnho

--- controllers/mapa.py
+++ controllers/mapa.py
@@ -1,17 +1,15 @@
 import json
 import random
 import os
+import math
 
+mapa_name="try"
 # Caminho base para os arquivos
-base_path = "C:\\Users\\ricar\\PycharmProjects\\cv-primitive-mapping\\worlds\\empty.wbt"
+base_path = "C:\\Users\\guiva\\Documents\\Aulas\\3ANO\\ir\\cv-primitive-mapping\\worlds\\empty.wbt"
+updated_map_path = f"C:\\Users\\guiva\\Documents\\Aulas\\3ANO\\ir\\cv-primitive-mapping\\worlds\\{mapa_name}.wbt"
+world_size = 0.5  # Metade do tamanho do mundo para facilitar os cálculos
 
-# Caminho para salvar o mapa atualizado
-updated_map_path = "C:\\Users\\ricar\\PycharmProjects\\cv-primitive-mapping\\worlds\\triangle2.wbt"
-
-# Tamanho do mundo (ajuste conforme necessário)
-world_size = 0.5
-
-# Função para criar a string de um retângulo
+# Funções para criar as strings das formas
 def create_rectangle_string(x, y, width, height, name):
     return f"""
 DEF {name} Solid {{
@@ -34,7 +32,6 @@ DEF {name} Solid {{
 }}
     """
 
-# Função para criar a string de um círculo
 def create_circle_string(x, y, radius, name):
     return f"""
 DEF {name} Solid {{
@@ -58,7 +55,6 @@ DEF {name} Solid {{
 }}
     """
 
-# Função para criar a string de um triângulo
 def create_triangle_string(x, y, base, height, name):
     # Ajusta a posição Z para alinhar a base do triângulo com o chão
     z_position = 0  # Isso centraliza o triângulo verticalmente ao redor de z=0, então subimos metade da altura
@@ -115,30 +111,30 @@ DEF {name} Solid {{
       geometry IndexedFaceSet {{
         coord Coordinate {{
           point [
-            0 {-radius} {height / 2},                           # Vértice 1 (topo)
-            {radius * 0.9} {-radius * 0.3} {height / 2},   # Vértice 2 (topo)
-            {radius * 0.5} {radius * 0.8} {height / 2},    # Vértice 3 (topo)
-            {-radius * 0.5} {radius * 0.8} {height / 2},   # Vértice 4 (topo)
+            0 {-radius} {height / 2},                        # Vértice 1 (topo)
+            {radius * 0.9} {-radius * 0.3} {height / 2},    # Vértice 2 (topo)
+            {radius * 0.5} {radius * 0.8} {height / 2},     # Vértice 3 (topo)
+            {-radius * 0.5} {radius * 0.8} {height / 2},    # Vértice 4 (topo)
             {-radius * 0.91} {-radius * 0.3} {height / 2},  # Vértice 5 (topo)
-            0 {-radius} {-height / 2},                          # Vértice 1 (baixo)
-            {radius * 0.9} {-radius * 0.3} {-height / 2},  # Vértice 2 (baixo)
-            {radius * 0.5} {radius * 0.8} {-height / 2},   # Vértice 3 (baixo)
-            {-radius * 0.5} {radius * 0.8} {-height / 2},  # Vértice 4 (baixo)
+            0 {-radius} {-height / 2},                      # Vértice 1 (baixo)
+            {radius * 0.9} {-radius * 0.3} {-height / 2},   # Vértice 2 (baixo)
+            {radius * 0.5} {radius * 0.8} {-height / 2},    # Vértice 3 (baixo)
+            {-radius * 0.5} {radius * 0.8} {-height / 2},   # Vértice 4 (baixo)
             {-radius * 0.91} {-radius * 0.3} {-height / 2}  # Vértice 5 (baixo)
           ]
         }}
         coordIndex [
-          0 1 2 3 4 -1,  # Face superior
-          1 0 6 5 -1,    # Lado 1
-          2 1 7 6 -1,    # Lado 2
-          3 2 8 7 -1,    # Lado 3
-          4 3 9 8 -1,    # Lado 4
-          4 0 5 9 -1,     # Lado 5
-          5 6 7 8 9 -1  # Face inferior
+          0 1 2 3 4 -1,    # Face superior
+          5 6 7 8 9 -1,    # Face inferior
+          0 5 6 1 -1,      # Lado 1
+          1 6 7 2 -1,      # Lado 2
+          2 7 8 3 -1,      # Lado 3
+          3 8 9 4 -1,      # Lado 4
+          4 9 5 0 -1       # Lado 5
         ]
         solid TRUE
       }}
-      castShadows FALSE
+      castShadows TRUE
       isPickable FALSE
     }}
   ]
@@ -146,54 +142,74 @@ DEF {name} Solid {{
 }}
 """
 
-# Lista de anotações para YOLO
-annotations = []
+# Função para selecionar a forma e sua posição
+def select_shape_and_position(shape_type, quadrant=None):
+    if shape_type == 'rectangle':
+        width = round(random.uniform(0.1, 0.3), 2)
+        height = round(random.uniform(0.1, 0.3), 2)
+        dimensions=(width,height)
+    elif shape_type == 'circle':
+        radius = round(random.uniform(0.05, 0.15), 2)
+        dimensions = (radius,)
+    elif shape_type == 'triangle':
+        base = round(random.uniform(0.1, 0.3), 2)
+        height = 0.1
+        dimensions = (base, height)
+    elif shape_type == 'pentagon':
+        radius = round(random.uniform(0.05, 0.1), 2)
+        height = 0.1
+        dimensions = (radius, height)
+    else:
+        raise ValueError("Unsupported shape type")
 
-# Escolhe aleatoriamente entre retângulo, círculo, triângulo ou pentágono
-shape_type = 'triangle'
+    if quadrant:
+        x_offset = (quadrant - 1) % 2 * world_size - world_size / 2
+        y_offset = (quadrant - 1) // 2 * world_size - world_size / 2
+        x = x_offset + random.uniform(-0.1, 0.1)
+        y = y_offset + random.uniform(-0.1, 0.1)
+    else:
+        x = round(random.uniform(-0.3, world_size - 0.2), 2)
+        y = round(random.uniform(-0.3, world_size - 0.2), 2)
 
-# Escolhe aleatoriamente as dimensões e a posição da forma
-x = round(random.uniform(0.1, world_size - 0.5), 2)
-y = round(random.uniform(0.1, world_size - 0.5), 2)
+    return shape_type, x, y, dimensions
+annotations=[]
+num_quadrants = 4 # Exemplo: O usuário quer figuras em 3 quadrantes
 
-if shape_type == 'rectangle':
-    width = round(random.uniform(0.3, 0.6), 2)
-    height = round(random.uniform(0.3, 0.6), 2)
-    shape_string = create_rectangle_string(x, y, width, height, "RECTANGLE")
-    annotation = {'name': "RECTANGLE", 'type': 'rectangle', 'x': x, 'y': y, 'width': width, 'height': height}
-
-elif shape_type == 'circle':
-    radius = round(random.uniform(0.05, 0.15), 2)
-    shape_string = create_circle_string(x, y, radius, "CIRCLE")
-    annotation = {'name': "CIRCLE", 'type': 'circle', 'x': x, 'y': y, 'radius': radius}
-
-elif shape_type == 'triangle':
-    base = round(random.uniform(0.1, 0.3), 2)
-    height = 0.1
-    shape_string = create_triangle_string(x, y, base, height, "TRIANGLE")
-    annotation = {'name': "TRIANGLE", 'type': 'triangle', 'x': x, 'y': y, 'base': base, 'height': height}
-
-elif shape_type == 'pentagon':
-    radius = round(random.uniform(0.05, 0.1), 2)  # Diminuindo o raio para um tamanho menor
-    height = 0.1
-    shape_string = create_pentagon_string(x, y, radius, height, "PENTAGON")
-    annotation = {'name': "PENTAGON", 'type': 'pentagon', 'x': x, 'y': y, 'radius': radius, 'height': height}
-
-annotations.append(annotation)
-print(annotation)
-# Lê o conteúdo do mapa vazio
+# Lê o conteúdo do mapa vazio apenas uma vez, antes do loop
 with open(base_path, 'r') as file:
     map_content = file.read()
 
-# Adiciona a forma ao conteúdo do mapa
-map_content_with_shape = map_content.strip() + "\n" + shape_string + "\n"
+map_content_with_shapes = map_content.strip()  # Prepara o conteúdo inicial
 
-# Salva o novo mapa com a forma adicionada
+# Processa cada quadrante escolhido
+for quadrant in range(1, num_quadrants+1):
+    shape_type = random.choice(['rectangle', 'circle', 'triangle', 'pentagon'])  # Escolhe uma forma aleatoriamente para cada quadrante
+    shape_type, x, y, dimensions = select_shape_and_position(shape_type, quadrant)
+    if shape_type == 'rectangle':
+        shape_string = create_rectangle_string(x, y, dimensions[0], dimensions[1], "RECTANGLE")
+        annotation = {'name': shape_type.upper(), 'type': 'rectangle', 'x': x, 'y': y, 'width': dimensions[0], 'height': dimensions[1]}
+    elif shape_type == 'circle':
+        shape_string = create_circle_string(x, y, dimensions[0], "CIRCLE")
+        annotation = {'name': shape_type.upper(), 'type': 'circle', 'x': x, 'y': y, 'radius': dimensions[0]}
+    elif shape_type == 'triangle':
+        shape_string = create_triangle_string(x, y, dimensions[0], dimensions[1], "TRIANGLE")
+        annotation = {'name': "TRIANGLE", 'type': 'triangle', 'x': x, 'y': y, 'base': dimensions[0], 'height': dimensions[1]}
+    elif shape_type == 'pentagon':
+        shape_string = create_pentagon_string(x, y, dimensions[0], dimensions[1], "PENTAGON")
+        annotation = {'name': shape_type.upper(), 'type': 'pentagon', 'x': x, 'y': y, 'radius': dimensions[0], 'height': dimensions[1]}
+
+    annotations.append(annotation)
+    print(annotation)
+
+    # Acumula cada forma no conteúdo do mapa
+    map_content_with_shapes += "\n" + shape_string + "\n"
+
+# Após o loop, salva todas as formas acumuladas de uma só vez no arquivo do mapa
 with open(updated_map_path, 'w') as file:
-    file.write(map_content_with_shape)
+    file.write(map_content_with_shapes)
 
 # Salva as anotações em formato JSON
-with open('annotations.json', 'w') as file:
+with open(f'{mapa_name}', 'w') as file:
     json.dump(annotations, file, indent=4)
 
 print("Mapa atualizado com sucesso!")
